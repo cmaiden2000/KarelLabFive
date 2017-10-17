680dbe240c3c8be1bf2812d9eea3c398e2fcaa50
cmaiden2000 <christophem750@gmail.com>
2017-10-17 12:36:29 -0700

    f

--- DemocracyBot.java
+++ DemocracyBot.java
@@ -12,8 +12,70 @@ public class DemocracyBot extends Robot
     }
     
     public void fixBallots() {
-        
+        move();
+        checkThirdStreet();
     }
-   
+    public boolean checkThirdStreet() {
+        move();
+        if (!nextToABeeper()) {
+            clearRooms();
+            move();
+            move();
+            checkThirdStreet();
+        }
+        else {
+            turnLeft();
+            move();
+            if (nextToABeeper()) {
+                turnLeft();
+                turnLeft();
+                move();
+                turnLeft();
+            }
+            else {
+                putBeeper();
+                turnLeft();
+                turnLeft();
+                move();
+                turnLeft();
+            }
+        }
+        move();
+        checkThirdStreet();
+        return true;
+    }
+    public boolean clearRooms() {
+        turnLeft();
+        move();
+        if (nextToABeeper()) {
+            pickBeeper();
+            turnLeft();
+            turnLeft();
+            move();
+            move();
+            pickBeeper();
+            turnLeft();
+            turnLeft();
+            move();
+            turnRight();
+        }
+        else if (!nextToABeeper()) {
+            turnLeft();
+            turnLeft();
+            move();
+            move();
+            pickBeeper();
+            turnLeft();
+            turnLeft();
+            move();
+            turnRight();
+        }
+        return true;
+    }
+    public void turnRight(){
+        turnLeft();
+        turnLeft();
+        turnLeft();
+    }
 }
 
